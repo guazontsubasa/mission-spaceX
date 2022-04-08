27ae5a86825b36bb35401d44d6f7a5a865f9285e
Leandro Pocai <leandropocai@gmail.com>
2022-04-08 17:05:05 -0300

    layout and routing

---
+++ .netlify/functions-internal/render.js
@@ -0,0 +1,32 @@
+const { init } = require('../handler.js');
+
+exports.handler = init({
+	appDir: "_app",
+	assets: new Set(["favicon.png"]),
+	mimeTypes: {".png":"image/png"},
+	_: {
+		entry: {"file":"start-4fc4c911.js","js":["start-4fc4c911.js","chunks/index-dd45c2ae.js"],"css":[]},
+		nodes: [
+			() => Promise.resolve().then(() => require('../server/nodes/0.js')),
+			() => Promise.resolve().then(() => require('../server/nodes/1.js')),
+			() => Promise.resolve().then(() => require('../server/nodes/2.js'))
+		],
+		routes: [
+			{
+				type: 'page',
+				id: "",
+				pattern: /^\/$/,
+				names: [],
+				types: [],
+				path: "/",
+				shadow: null,
+				a: [0,2],
+				b: [1]
+			}
+		],
+		matchers: async () => {
+			
+			return {  };
+		}
+	}
+});
--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,32 @@
-import adapter from '@sveltejs/adapter-auto';
+// import adapter from '@sveltejs/adapter-auto';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
+// /** @type {import('@sveltejs/kit').Config} */
+// const config = {
+// 	kit: {
+// 		adapter: adapter(),
+// 		split: false,
+// 		prerender : { default: true}
+// 	}
+// };
+
+// export default config;
+
+// svelte.config.js
+import adapter from '@sveltejs/adapter-static';
+
+export default {
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			// default options are shown
+			pages: 'build',
+			assets: 'build',
+			fallback: null,
+			precompress: false
+		}),
+
+		prerender: {
+			// This can be false if you're using a fallback (i.e. SPA mode)
+			default: true
+		}
 	}
 };
-
-export default config;
